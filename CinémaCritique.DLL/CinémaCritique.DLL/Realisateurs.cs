009627740add607c647db8d3321e9bece0f93e3a
Zestaria-fr <monteilletarthur@gmail.com>
2026-09-22 17:00:09 +0200

    Create Realisateurs.cs

---
+++ CinémaCritique.DLL/CinémaCritique.DLL/Realisateurs.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CinémaCritique.DLL
+{
+    internal class Realisateurs
+    {
+        #region Champs privés
+
+        private string _nom = "inconnu";
+        private string _prenom = "inconnu";
+        private string _dateNaissance = "inconnu";
+        private string _nationalite = "inconnu";
+        private string _sexe = "inconnu";
+        private string _consecration = "inconnu";
+
+        #endregion
+        #region Propriétés
+
+        #endregion
+        #region Constructeurs
+
+        #endregion
+        #region Accesseurs (getteurs / setteurs)
+
+        #endregion
+        #region Méthodes
+
+        #endregion
+
+
+
+
+
+
+    }
+}
