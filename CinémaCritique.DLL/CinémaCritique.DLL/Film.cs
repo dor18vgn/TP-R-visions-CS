a7be6accc390695a4c2a68e98f10d6989cd212db
Zestaria-fr <monteilletarthur@gmail.com>
2026-09-23 14:11:28 +0200

    CinemaCritique

--- CinémaCritique.DLL/CinémaCritique.DLL/Film.cs
+++ CinémaCritique.DLL/CinémaCritique.DLL/Film.cs
@@ -4,27 +4,203 @@ using System.Text;
 
 namespace CinémaCritique.DLL
 {
-    internal class Film
+    public enum Film
     {
-        #region Champs privés
+        Metropolis,
 
-        private string _nom = "inconnu";
-        private string _nationalite = "inconnu";
-         
+        Spider_Man_No_Way_Home,
 
-        #endregion
-        #region Propriétés
+        Avengers_Endgame,
 
-        #endregion
-        #region Constructeurs
+        Titanic,
 
-        #endregion
-        #region Accesseurs (getteurs / setteurs)
+        Avatar,
 
-        #endregion
-        #region Méthodes
+        Harry_Potter_a_l_ecole_des_sorciers,
 
-        #endregion
+        Inception,
+
+        Interstellar,
+
+        The_Dark_Knight,
+
+        Oppenheimer,
+
+        Barbie,
+
+        Le_Loup_de_Wall_Street,
+
+        Pirates_des_Caraibes_la_Malediction_du_Black_Pearl,
+
+        Hunger_Games,
+
+        Matrix,
+
+        Fight_Club,
+
+        Pulp_Fiction,
+
+        Forrest_Gump,
+
+        Gladiator,
+
+        Dune,
+
+        L_Ile_Interdite_Shutter_Island,
+
+        Once_Upon_a_Time_in_Hollywood,
+
+        La_La_Land,
+
+        Joker,
+
+        Fast_and_Furious_7,
+
+        Creed,
+
+        Mission_Impossible_Fallout,
+
+        Batman_v_Superman,
+
+        Top_Gun_Maverick,
+
+        Venom,
+
+        Uncharted,
+
+        John_Wick,
+
+        Transformers,
+
+        Jurassic_World,
+
+        Bohemian_Rhapsody,
+
+        The_Batman,
+
+        Deadpool,
+
+        Iron_Man,
+
+        Thor_Ragnarok,
+
+        Black_Panther,
+
+        Doctor_Strange,
+
+        Les_Gardiens_de_la_Galaxie,
+
+        Tenet,
+
+        Suicide_Squad,
+
+        Men_in_Black,
+
+        Le_Labyrinthe,
+
+        Divergente,
+
+        Nos_etoiles_contraires,
+
+        A_travers_ma_fenetre,
+
+        Twilight,
+
+        Fifty_Shades_of_Grey,
+
+        The_Social_Network,
+
+        Catch_Me_If_You_Can,
+
+        Django_Unchained,
+
+        Inglourious_Basterds,
+
+        Le_Gatsby_magnifique,
+
+        Shining,
+
+        Scream,
+
+        Conjuring,
+
+        M3GAN,
+
+        Five_Nights_at_Freddy_s,
+
+        Un_havre_de_paix,
+
+        Le_Diable_s_habille_en_Prada,
+
+        Mean_Girls,
+
+        High_School_Musical,
+
+        Pitch_Perfect,
+
+        Babylon,
+
+        Wonka,
+
+        Elvis,
+
+        Knives_Out,
+
+        Glass_Onion,
+
+        Enola_Holmes,
+
+        Red_Notice,
+
+        The_Gray_Man,
+
+        Extraction,
+
+        Bird_Box,
+
+        Project_X,
+
+        Superbad,
+
+        Very_Bad_Trip,
+
+        twenty_one_Jump_Street,
+
+        Ted,
+
+        Free_Guy,
+
+        Jumanji_Bienvenue_dans_la_jungle,
+
+        Un_p_tit_truc_en_plus,
+
+        Intouchables,
+
+        Astérix_et_Obélix_L_Empire_du_Milieu,
+
+        BAC_Nord,
+
+        La_Haine,
+
+        Le_Comte_de_Monte_Cristo,
+
+        Novembre,
+
+        Boite_Noire,
+
+        L_Arnacoeur,
+
+        Taxi,
+
+        Qu_est_ce_qu_on_a_fait_au_Bon_Dieu,
+
+        Le_Loup_et_le_Lion,
+
+        Simone_le_voyage_du_siecle,
+
+        Anatomie_d_une_chute,
+
+        Les_Trois_Mousquetaires_D_Artagnan
 
 
     }
