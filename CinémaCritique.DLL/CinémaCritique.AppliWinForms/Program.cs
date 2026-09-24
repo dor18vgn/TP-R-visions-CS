7e13422486a9f6679a67bfbf3fb6405ae944908e
Zestaria-fr <monteilletarthur@gmail.com>
2026-09-24 09:09:56 +0200

    CritiqueCine

--- CinémaCritique.DLL/CinémaCritique.AppliWinForms/Program.cs
+++ CinémaCritique.DLL/CinémaCritique.AppliWinForms/Program.cs
@@ -20,12 +20,13 @@ namespace CinémaCritique.AppliWinForms
             Application.SetCompatibleTextRenderingDefault(false);
             //Application.Run(new Form1());
 
-          Acteur JakeGyllenhaal = new Acteur("Gyllenhaal", "Jake", new DateTime(1980, 12, 19), "Américain", "Homme", new List<Film>());
+            Acteur JakeGyllenhaal = new Acteur("Gyllenhaal", "Jake", new DateTime(1980, 12, 19), "Américain", "Homme", new List<Film>());
             Acteur RyanGosling = new Acteur("Gosling", "Ryan", new DateTime(1980, 11, 12), "Américain", "Homme", new List<Film>());
             Realisateur RichardKelly = new Realisateur("Kelly", "Richard", new DateTime(1970, 03, 24), "Américain", "Homme", "Récompensé aux Oscars", new List<Film>());
-          Film DonnieDarko = new Film("Donnie Darko", new DateTime(2001, 10, 26), "Américain", new List<Acteur> { JakeGyllenhaal }, new List<Realisateur> { RichardKelly }, 8.0f);
+         
+            Film DonnieDarko = new Film("Donnie Darko", new DateTime(2001, 10, 26), "Américain", new List<Acteur> { JakeGyllenhaal }, new List<Realisateur> { RichardKelly }, 8.0f);
 
-           
+         
 
         }
     }
